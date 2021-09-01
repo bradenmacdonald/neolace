793e3370ad92b3e39cc8083a5b1dc6ef466bd742
Braden MacDonald <mail@bradenm.com>
2021-08-31 21:01:42 -0700

    Update 404 page

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,15 +1,17 @@
-import { Page } from 'components/Page';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-export default function Home() {
-    return (
-        <Page
-            title="Page Not Found (404) - TechNotes"
-        >
-            <h1>Page Not Found (Error 404)</h1>
+export default function FourOhFour() {
 
-            <p className="description">
-                Sorry, the requested page could not be found.
-            </p>
-        </Page>
-    );
+    const router = useRouter();
+
+    return <>
+        <Head>
+            <title>Page Not Found (404)</title>
+        </Head>
+        <main className="py-12 px-4 mx-auto max-w-md">
+            <h1 className="font-bold text-3xl mb-3">⚠️ Not Found</h1>
+            <p>Sorry, the page you requested was not found.</p>
+        </main>
+    </>;
 }
