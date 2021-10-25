568b0bb0e968939b24d99b7e9bc1fe06d85a9dd6
Braden MacDonald <mail@bradenm.com>
2021-10-25 11:16:06 -0700

    Upgrade computed-types to 1.11.1, which brings record support + bugfixes

--- neolace-api/src/deps/computed-types/schema/FunctionType.ts
+++
@@ -1,6 +0,0 @@
-// imports rewritten with <3 from denoporter - https://github.com/SirJosh3917/denoporter
-
-export type FunctionParameters = unknown[];
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FunctionType<R = any, P extends FunctionParameters = any[]> = (...args: P) => R;
-export default FunctionType;
