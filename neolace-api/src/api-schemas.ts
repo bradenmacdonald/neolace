9a752596a4a873c3232aed5ebcdf90ca22c05e32
Braden MacDonald <mail@bradenm.com>
2021-10-29 11:57:04 -0700

    Revert "Upgrade computed-types to 1.11.1" (some frontend types were missing)

--- neolace-api/src/api-schemas.ts
+++ neolace-api/src/api-schemas.ts
@@ -23,6 +23,14 @@ export const nullable = <T>(validator: T) => Schema.either(unknown.equals(null),
 
 export const vnidString = string.regexp(/^_[0-9A-Za-z]{1,22}$/).transform(x => x as VNID);
 
+// Until computed_types supports a Record type, we have our own. See https://github.com/neuledge/computed-types/issues/43
+export const Record = <KeyType extends string | number | symbol, ValueType>(keySchema: Validator<KeyType>, valueSchema: Validator<ValueType>) => {
+    return (object
+        .test((obj) => Object.values(obj).every(valueSchema) && Object.keys(obj).every(keySchema))
+        .transform(obj => obj as Record<KeyType, ValueType>)
+    );
+}
+
 // Most string types have the following validation:
 export const normalString = string.trim().max(1_000);
 
