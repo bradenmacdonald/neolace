14d5bae0ea75ab61aa9f7c68aedcc41b29e7f1df
Braden MacDonald <mail@bradenm.com>
2021-11-24 13:13:08 -0500

    Security fix for getting an entry

--- backend/neolace/core/lookup/values.ts
+++ backend/neolace/core/lookup/values.ts
@@ -5,7 +5,9 @@ import {
     VNID,
 } from "neolace/deps/vertex-framework.ts";
 import * as api from "neolace/deps/neolace-api.ts";
+import { Site } from "neolace/core/Site.ts";
 import { Entry } from "neolace/core/entry/Entry.ts";
+import { EntryType } from "neolace/core/schema/EntryType.ts";
 
 import type { LookupContext } from "./context.ts";
 import { LookupError, LookupEvaluationError } from "./errors.ts";
@@ -226,7 +228,7 @@ export class EntryValue extends ConcreteValue implements IHasLiteralExpression {
     protected override doCastTo(newType: ClassOf<LookupValue>, context: LookupContext): LookupValue|undefined {
         if (newType === LazyEntrySetValue) {
             return new LazyEntrySetValue(context, C`
-                MATCH (entry:${Entry} {id: ${this.id}})
+                MATCH (entry:${Entry} {id: ${this.id}})-[:${Entry.rel.IS_OF_TYPE}]->(et:${EntryType})-[:${EntryType.rel.FOR_SITE}]->(:${Site} {id: ${context.siteId}})
                 WITH entry, {} AS annotations
             `);
         } else if (newType === AnnotatedEntryValue) {
