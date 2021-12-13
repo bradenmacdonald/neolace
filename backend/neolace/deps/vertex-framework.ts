94a6f0b0006680078f1962ed9b461e4f426c9019
Braden MacDonald <mail@bradenm.com>
2021-12-13 15:19:26 -0800

    Fixes

--- backend/neolace/deps/vertex-framework.ts
+++ backend/neolace/deps/vertex-framework.ts
@@ -1,1 +1,3 @@
-export * from "../../../../vertex-framework/vertex/index.ts";
+// For local dev:
+// export * from "../../../../vertex-framework/vertex/index.ts";
+export * from "https://raw.githubusercontent.com/neolace-dev/vertex-framework/main/vertex/index.ts";
