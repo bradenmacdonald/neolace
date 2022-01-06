dd648ed45119de2656ce17b567ebf938246b9d5d
Braden MacDonald <mail@bradenm.com>
2022-01-06 14:56:39 -0800

    Move TechNotes example content to a separate repository

--- backend/neolace/core/objstore/detect-metadata.test.ts
+++ backend/neolace/core/objstore/detect-metadata.test.ts
@@ -1,22 +1,21 @@
 import { group, test, assertEquals } from "neolace/lib/tests.ts";
-import { getFullPath } from "neolace/sample-data/technotes/datafiles.ts";
-import { files } from "../../sample-data/technotes/datafiles.ts";
+import { getFullPath, files } from "neolace/sample-data/plantdb/datafiles.ts";
 import { detectImageMetadata } from "./detect-metadata.ts";
 
 
 group(import.meta, () => {
 
     test("it can detect an image's size", async () => {
-        const imageDataWebp = await Deno.readFile(getFullPath(files.miniCooperSe.path));
+        const imageDataWebp = await Deno.readFile(getFullPath(files.ponderosaPineImg.path));
         const metadata = await detectImageMetadata(imageDataWebp);
         assertEquals(metadata.type, "image");
-        assertEquals(metadata.width, 4054);
-        assertEquals(metadata.height, 2216);
+        assertEquals(metadata.width, 3504);
+        assertEquals(metadata.height, 2336);
     });
     test("it can generate a blurHash for an image", async () => {
-        const imageDataWebp = await Deno.readFile(getFullPath(files.miniCooperSe.path));
+        const imageDataWebp = await Deno.readFile(getFullPath(files.ponderosaPineImg.path));
         const metadata = await detectImageMetadata(imageDataWebp);
         assertEquals(metadata.type, "image");
-        assertEquals(metadata.blurHash, "LWF=:f%2xaxu~qxts:floLj?xbM{");
+        assertEquals(metadata.blurHash, "LCDu}B~VNu9Z0LxGNH9u$zjYWCt7");
     });
 });
--- backend/neolace/core/objstore/objstore.test.ts
+++ backend/neolace/core/objstore/objstore.test.ts
@@ -1,16 +1,15 @@
 import { group, test, assertEquals } from "neolace/lib/tests.ts";
-import { getFullPath } from "neolace/sample-data/technotes/datafiles.ts";
-import { files } from "../../sample-data/technotes/datafiles.ts";
+import { getFullPath, files } from "neolace/sample-data/plantdb/datafiles.ts";
 import { uploadFileToObjStore } from "./objstore.ts";
 
 
 group(import.meta, () => {
 
     test("it can upload an image file into object storage", async () => {
-        const imageDataWebp = await Deno.open(getFullPath(files.miniCooperSe.path));
+        const imageDataWebp = await Deno.open(getFullPath(files.ponderosaPineImg.path));
         const uploadResult = await uploadFileToObjStore(imageDataWebp, {contentType: "image/webp"});
         imageDataWebp.close();
-        assertEquals(uploadResult.size, 439352);
-        assertEquals(uploadResult.sha256Hash, "552b46ae2c456ee5c76c2ab0e092b0bc4fde36c984b030ad887adaf57c513fdb");
+        assertEquals(uploadResult.size, 1581898);
+        assertEquals(uploadResult.sha256Hash, "e0b56bc58b5b4ac8b6f9a3e0fc5083d4c7f447738ef39241377687d6bfcef0e6");
     });
 });
