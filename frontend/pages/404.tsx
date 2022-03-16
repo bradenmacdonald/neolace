adcd53432653dcc7bf2d1d89d22f64329f80d496
Braden MacDonald <mail@bradenm.com>
2022-03-16 12:21:43 -0700

    Fix a warning about internationalization

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -3,7 +3,10 @@ import { FormattedMessage } from 'react-intl';
 
 export default function FourOhFour() {
 
-    const notFound = <FormattedMessage id="error.notFound" defaultMessage="Not Found" />;
+    // TODO: figure out how to localize the 404 page; currently it's static and shared for all sites, and the intlProvider
+    // is not in the React tree. Some of the logic from _app.tsx for loading locale data may have to be factored out and
+    // made to work here too.
+    const notFound = "Not Found";//<FormattedMessage id="error.notFound" defaultMessage="Not Found" />;
 
     return <>
         <Head>
@@ -11,7 +14,7 @@ export default function FourOhFour() {
         </Head>
         <main className="py-12 px-4 mx-auto max-w-md">
             <h1 className="font-bold text-3xl mb-3">⚠️ {notFound}</h1>
-            <p><FormattedMessage id="error.notFoundDetails" defaultMessage="Sorry, the page you requested was not found." /></p>
+            <p>Sorry, the page you requested was not found{/*<FormattedMessage id="error.notFoundDetails" defaultMessage="" />*/}</p>
         </main>
     </>;
 }
