2f35ee74a31b39c14c6b8552dc0448aa53e72c76
Braden MacDonald <mail@bradenm.com>
2022-03-29 12:14:47 -0700

    Move tooltips into a portal

---
+++ frontend/components/utils/Portal.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+interface Props {
+    className?: string;
+    el?: string;
+    children: React.ReactNode;
+}
+
+/**
+ * Use a React portal to render elements at the end of the <body>. Based on https://stackoverflow.com/a/59154364
+ * @param children Child elements
+ * @param className CSS classname
+ * @param el HTML element to create.  default: div
+ */
+export const Portal : React.FC<Props> = ( { children, className = 'fixed root-portal top-0 left-0 w-full', el = 'div' } ) => {
+
+    const [container, setContainer] = React.useState<HTMLElement|null>(null);
+
+    React.useEffect(() => {
+        // deno-lint-ignore no-explicit-any
+        const portals: Record<string, {element: HTMLElement, usageCount: number}> = (window as any)._portals || ((window as any)._portals = {});
+        const portalKey = el + ":" + className;
+        if (portals[portalKey] === undefined) {
+            portals[portalKey] = {element: document.createElement(el), usageCount: 1};
+            document.body.appendChild(portals[portalKey].element);
+            portals[portalKey].element.className = className;
+        } else {
+            portals[portalKey].usageCount++;
+        }
+        setContainer(portals[portalKey].element);
+        // Cleanup function: remove the element:
+        return () => {
+            if (portals[portalKey].usageCount-- <= 0) {
+                document.body.removeChild(portals[portalKey].element);
+            }
+        }
+    }, [])
+
+    return container ? ReactDOM.createPortal(children, container) : null;
+}
--- frontend/components/widgets/Tooltip.tsx
+++ frontend/components/widgets/Tooltip.tsx
@@ -1,3 +1,4 @@
+import { Portal } from 'components/utils/Portal';
 import React from 'react';
 import { usePopper } from 'react-popper';
 
@@ -61,17 +62,19 @@ export const Tooltip: React.FunctionComponent<TooltipProps> = (props) => {
                 "aria-describedby": tooltipId,
             }) : null}
 
-            <span
-                role="tooltip"
-                id={tooltipId}
-                ref={setPopperElement}
-                style={styles.popper}
-                {...attributes.popper}
-                className={`max-w-[400px] border p-1 rounded border-gray-800 shadow bg-blue-50 text-sm ${showTooltip ? "visible opacity-100" : "invisible opacity-0"} transition-opacity duration-500 font-normal z-10`}
-                aria-hidden={!showTooltip}
-            >
-                {showTooltip ? props.tooltipContent : null}
-            </span>
+            <Portal>
+                <span
+                    role="tooltip"
+                    id={tooltipId}
+                    ref={setPopperElement}
+                    style={styles.popper}
+                    {...attributes.popper}
+                    className={`max-w-[400px] border p-1 rounded border-gray-800 shadow bg-blue-50 text-sm ${showTooltip ? "visible opacity-100" : "invisible opacity-0"} transition-opacity duration-500 font-normal z-10`}
+                    aria-hidden={!showTooltip}
+                >
+                    {showTooltip ? props.tooltipContent : null}
+                </span>
+            </Portal>
         </>
     );
 };
