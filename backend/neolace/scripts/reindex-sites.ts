d7162ecceb1ba124989081858578b315dad7867d
Braden MacDonald <mail@bradenm.com>
2022-05-05 16:16:57 -0700

    Fix script to update search index

--- backend/neolace/scripts/reindex-sites.ts
+++ backend/neolace/scripts/reindex-sites.ts
@@ -1,9 +1,9 @@
-import { graph } from "neolace/core/graph.ts";
+import { getGraph } from "neolace/core/graph.ts";
 import { Site } from "neolace/core/Site.ts";
 import { reindexAllEntries } from "neolace/plugins/search/update-index.ts";
 import { shutdown } from "neolace/app/shutdown.ts";
 
-const sites = await graph.pull(Site, (s) => s.id.name);
+const sites = await (await getGraph()).pull(Site, (s) => s.id.name);
 for (const site of sites) {
     console.log(`Updating site ${site.name} (${site.id})`);
     await reindexAllEntries(site.id);
