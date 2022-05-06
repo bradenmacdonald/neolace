cf5154cdd4000f37950a16a56078ca963b133643
Konstantin Mestnikov <64804506+ritec03@users.noreply.github.com>
2022-05-06 10:54:25 -0700

    fixed test case and getValue function

--- backend/neolace/core/lookup/expressions/graph.test.ts
+++ backend/neolace/core/lookup/expressions/graph.test.ts
@@ -1,14 +1,8 @@
-import { VNID } from "neolace/deps/vertex-framework.ts";
-import { PropertyType } from "neolace/deps/neolace-api.ts";
 import { assertEquals, group, setTestIsolation, test } from "neolace/lib/tests.ts";
-import { graph } from "neolace/core/graph.ts";
-import { CreateSite } from "neolace/core/Site.ts";
-import { ApplyEdits } from "neolace/core/edit/ApplyEdits.ts";
-import { Ancestors, AndAncestors } from "./ancestors.ts";
-import { AnnotatedValue, GraphValue, IntegerValue, MakeAnnotatedEntryValue, PageValue } from "../values.ts";
+import { getGraph } from "neolace/core/graph.ts";
+import { AndAncestors } from "./ancestors.ts";
+import { GraphValue } from "../values.ts";
 import { This } from "./this.ts";
-import { Count } from "./count.ts";
-import { LookupExpression } from "../expression.ts";
 import { Graph } from "./graph.ts";
 
 group(import.meta, () => {
@@ -22,89 +16,93 @@ group(import.meta, () => {
             // this is the same as this.ancestors().graph()
             const expression = new Graph(new AndAncestors(new This()));
 
+            const graph = await getGraph();
             const value = await graph.read((tx) =>
                 expression.getValue({ tx, siteId, entryId: ponderosaPine.id, defaultPageSize: 10n }).then((v) =>
                     v.makeConcrete()
                 )
             );
 
-            assertEquals(value, {
-                entries: [
-                    {
-                        data: {},
-                        entryId: defaultData.entries.ponderosaPine.id,
-                        entryType: defaultData.schema.entryTypes._ETSPECIES.id,
-                        name: "Ponderosa Pine",
-                    },
-                    {
-                        data: {},
-                        entryId: defaultData.entries.genusPinus.id,
-                        entryType: defaultData.schema.entryTypes._ETGENUS.id,
-                        name: "Pinus",
-                    },
-                    {
-                        data: {},
-                        entryId: defaultData.entries.familyPinaceae.id,
-                        entryType: defaultData.schema.entryTypes._ETFAMILY.id,
-                        name: "Pinaceae",
-                    },
-                    {
-                        data: {},
-                        entryId: defaultData.entries.orderPinales.id,
-                        entryType: defaultData.schema.entryTypes._ETORDER.id,
-                        name: "Pinales",
-                    },
-                    {
-                        data: {},
-                        entryId: defaultData.entries.classPinopsida.id,
-                        entryType: defaultData.schema.entryTypes._ETCLASS.id,
-                        name: "Pinopsida",
-                    },
-                    {
-                        data: {},
-                        entryId: defaultData.entries.divisionTracheophyta.id,
-                        entryType: defaultData.schema.entryTypes._ETDIVISION.id,
-                        name: "Tracheophyta",
-                    },
-                ],
-                rels: [
-                    {
-                        data: {},
-                        fromEntryId: defaultData.entries.classPinopsida.id,
-                        relId: (value as GraphValue).rels[4]?.relId,
-                        relType: defaultData.schema.properties._parentDivision.id,
-                        toEntryId: defaultData.entries.divisionTracheophyta.id,
-                    },
-                    {
-                        data: {},
-                        fromEntryId: defaultData.entries.orderPinales.id,
-                        relId: (value as GraphValue).rels[3]?.relId,
-                        relType: defaultData.schema.properties._parentClass.id,
-                        toEntryId: defaultData.entries.classPinopsida.id,
-                    },
-                    {
-                        data: {},
-                        fromEntryId: defaultData.entries.familyPinaceae.id,
-                        relId: (value as GraphValue).rels[2]?.relId,
-                        relType: defaultData.schema.properties._parentOrder.id,
-                        toEntryId: defaultData.entries.orderPinales.id,
-                    },
-                    {
-                        data: {},
-                        fromEntryId: defaultData.entries.genusPinus.id,
-                        relId: (value as GraphValue).rels[1]?.relId,
-                        relType: defaultData.schema.properties._parentFamily.id,
-                        toEntryId: defaultData.entries.familyPinaceae.id,
-                    },
-                    {
-                        data: {},
-                        fromEntryId: defaultData.entries.ponderosaPine.id,
-                        relId: (value as GraphValue).rels[0]?.relId,
-                        relType: defaultData.schema.properties._parentGenus.id,
-                        toEntryId: defaultData.entries.genusPinus.id,
-                    },
-                ],
-            });
+            assertEquals(
+                value,
+                new GraphValue(
+                    [
+                        {
+                            data: {},
+                            entryId: defaultData.entries.ponderosaPine.id,
+                            entryType: defaultData.schema.entryTypes._ETSPECIES.id,
+                            name: "Ponderosa Pine",
+                        },
+                        {
+                            data: {},
+                            entryId: defaultData.entries.genusPinus.id,
+                            entryType: defaultData.schema.entryTypes._ETGENUS.id,
+                            name: "Pinus",
+                        },
+                        {
+                            data: {},
+                            entryId: defaultData.entries.familyPinaceae.id,
+                            entryType: defaultData.schema.entryTypes._ETFAMILY.id,
+                            name: "Pinaceae",
+                        },
+                        {
+                            data: {},
+                            entryId: defaultData.entries.orderPinales.id,
+                            entryType: defaultData.schema.entryTypes._ETORDER.id,
+                            name: "Pinales",
+                        },
+                        {
+                            data: {},
+                            entryId: defaultData.entries.classPinopsida.id,
+                            entryType: defaultData.schema.entryTypes._ETCLASS.id,
+                            name: "Pinopsida",
+                        },
+                        {
+                            data: {},
+                            entryId: defaultData.entries.divisionTracheophyta.id,
+                            entryType: defaultData.schema.entryTypes._ETDIVISION.id,
+                            name: "Tracheophyta",
+                        },
+                    ],
+                    [
+                        {
+                            data: {},
+                            fromEntryId: defaultData.entries.classPinopsida.id,
+                            relId: (value as GraphValue).rels[0]?.relId,
+                            relType: defaultData.schema.properties._parentDivision.id,
+                            toEntryId: defaultData.entries.divisionTracheophyta.id,
+                        },
+                        {
+                            data: {},
+                            fromEntryId: defaultData.entries.orderPinales.id,
+                            relId: (value as GraphValue).rels[1]?.relId,
+                            relType: defaultData.schema.properties._parentClass.id,
+                            toEntryId: defaultData.entries.classPinopsida.id,
+                        },
+                        {
+                            data: {},
+                            fromEntryId: defaultData.entries.familyPinaceae.id,
+                            relId: (value as GraphValue).rels[2]?.relId,
+                            relType: defaultData.schema.properties._parentOrder.id,
+                            toEntryId: defaultData.entries.orderPinales.id,
+                        },
+                        {
+                            data: {},
+                            fromEntryId: defaultData.entries.genusPinus.id,
+                            relId: (value as GraphValue).rels[3]?.relId,
+                            relType: defaultData.schema.properties._parentFamily.id,
+                            toEntryId: defaultData.entries.familyPinaceae.id,
+                        },
+                        {
+                            data: {},
+                            fromEntryId: defaultData.entries.ponderosaPine.id,
+                            relId: (value as GraphValue).rels[4]?.relId,
+                            relType: defaultData.schema.properties._parentGenus.id,
+                            toEntryId: defaultData.entries.genusPinus.id,
+                        },
+                    ],
+                ),
+            );
         });
     });
 });
--- backend/neolace/core/lookup/expressions/graph.ts
+++ backend/neolace/core/lookup/expressions/graph.ts
@@ -3,7 +3,9 @@ import { Entry } from "neolace/core/entry/Entry.ts";
 import { LookupExpression } from "../expression.ts";
 import { GraphValue, LazyEntrySetValue } from "../values.ts";
 import { LookupContext } from "../context.ts";
-import { C, Field, VNID } from "neolace/deps/vertex-framework.ts";
+import { C, Field } from "neolace/deps/vertex-framework.ts";
+import { PropertyFact } from "../../entry/PropertyFact.ts";
+import { EntryType } from "../../schema/EntryType.ts";
 
 /**
  * graph([entry or entry set])
@@ -29,12 +31,21 @@ export class Graph extends LookupExpression {
             ${entrySetQuery.cypherQuery}
             WITH collect(entry) AS entries
             MATCH (e1:${Entry})-[rel:${Entry.rel.RELATES_TO}|${Entry.rel.IS_A}]->(e2:${Entry}) WHERE e1 IN entries AND e2 IN entries
-            WITH collect(rel {start: startNode(rel).id, end: endNode(rel).id}) AS rels, entries
-            RETURN rels, entries
+            // Now we have the relationships between the entries, but key data about each relationship is stored
+            // on the corresponding PropertyFact and Property nodes, not on the relationship itself, so fetch those too:
+            MATCH (pf:${PropertyFact} {directRelNeo4jId: id(rel)})-[:${PropertyFact.rel.FOR_PROP}]->(prop)
+            WITH collect(rel {start: startNode(rel).id, end: endNode(rel).id, relId: pf.id, relType: prop.id}) AS rels, entries
+            WITH rels, entries
+            // Add the entry type information to the entries we are returning:
+            UNWIND entries AS entry
+            MATCH (entry)-[:${Entry.rel.IS_OF_TYPE}]->(et:${EntryType})
+            RETURN rels, collect(entry {.id, .name, type: et.id}) AS entries
         `.givesShape(
             {
-                rels: Field.List(Field.Record({ start: Field.VNID, end: Field.VNID })),
-                entries: Field.List(Field.VNode(Entry)),
+                rels: Field.List(
+                    Field.Record({ start: Field.VNID, end: Field.VNID, relId: Field.VNID, relType: Field.VNID }),
+                ),
+                entries: Field.List(Field.Record({ id: Field.VNID, name: Field.String, type: Field.VNID })),
             },
         ));
 
@@ -42,15 +53,15 @@ export class Graph extends LookupExpression {
             return {
                 entryId: entry.id,
                 name: entry.name,
-                entryType: VNID(),
+                entryType: entry.type,
                 data: {},
             };
         });
 
         const relationships = graphData.rels.map((rel) => {
             return {
-                relId: VNID(),
-                relType: VNID(),
+                relId: rel.relId,
+                relType: rel.relType,
                 fromEntryId: rel.start,
                 toEntryId: rel.end,
                 data: {},
