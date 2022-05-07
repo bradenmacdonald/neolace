bd2a44b665110be0ad16a6b5754df8a68fc82ce6
Konstantin Mestnikov <64804506+ritec03@users.noreply.github.com>
2022-05-06 17:08:32 -0700

    improved graph visualization.

--- frontend/components/Graph.tsx
+++ frontend/components/Graph.tsx
@@ -4,8 +4,7 @@
 import React from 'react';
 import { api } from 'lib/api-client';
 import { MDTContext } from './markdown-mdt/mdt';
-import G6, { Graph } from '@antv/g6';
-import ReactDOM from 'react-dom';
+import G6, { Graph, IG6GraphEvent, NodeConfig } from '@antv/g6';
 
 interface GraphProps {
     value: api.GraphValue;
@@ -13,15 +12,51 @@ interface GraphProps {
     children?: never;
 }
 
-function convertValueToDate(value: api.GraphValue) {
-    return {
+const listOfColours = [
+    "red",
+    "pink",
+    "skyblue",
+    "orange",
+    "green",
+    "violet",
+]
+
+const colour_dict = new Map()
+
+
+function convertValueToData(value: api.GraphValue) {
+    const data = {
         nodes: value.entries.map((n) => (
-            { id: n.entryId, label: n.name }
+            { id: n.entryId, label: n.name , entryType: n.entryType}
         )),
         edges: value.rels.map((e) => (
-            { source: e.fromEntryId, target: e.toEntryId }
+            { source: e.fromEntryId, target: e.toEntryId, entryType: e.relType }
         )),
     }
+
+    data.nodes.forEach((node: NodeConfig) => {
+        node.type = 'rect';
+
+        if (!colour_dict.has(node.entryType)) {
+            colour_dict.set(node.entryType, listOfColours.pop());
+        }
+        const colour =  colour_dict.get(node.entryType);
+
+        node.style = {
+            // The style for the keyShape
+            fill: colour,
+            stroke: '#888',
+            lineWidth: 1,
+        };
+    })
+
+    return data;
+}
+
+function refreshDragedNodePosition(e: IG6GraphEvent) {
+    const model = e.item!.get('model');
+    model.fx = e.x;
+    model.fy = e.y;
 }
 
 /**
@@ -29,46 +64,51 @@ function convertValueToDate(value: api.GraphValue) {
  */
 export const LookupGraph: React.FunctionComponent<GraphProps> = (props) => {
     const data = React.useMemo(() => {
-        return convertValueToDate(props.value);
+        return convertValueToData(props.value);
     }, [props.value])
 
     const ref = React.useRef<HTMLDivElement>(null);
     const [graph, setGraph] = React.useState<Graph | null>(null);
 
+
     React.useEffect(() => {
         if (!graph && ref.current) {
+            const container = ref.current;
+            const width = container.scrollWidth;
+            const height = container.scrollHeight || 500;
             console.log("Here")
             const newGraph = new G6.Graph({
-                container: ref.current,
-                width: 1200,
-                height: 2000,
-                modes: {
-                    default: ['drag-canvas'],
-                },
+                container,
+                width,
+                height,
                 layout: {
-                    type: 'dagre',
-                    direction: 'LR',
+                    type: 'force',
                 },
                 defaultNode: {
-                    type: 'node',
-                    labelCfg: {
-                        style: {
-                            fill: '#000000A6',
-                            fontSize: 10,
-                        },
-                    },
-                    style: {
-                        stroke: '#72CC4A',
-                        width: 150,
-                    },
-                },
-                defaultEdge: {
-                    type: 'polyline',
+                    size: 15,
                 },
             });
             newGraph.data(data);
             newGraph.render();
             setGraph(newGraph);
+
+            newGraph.on('node:dragstart', function (e) {
+                newGraph.layout();
+                refreshDragedNodePosition(e);
+            });
+            newGraph.on('node:drag', function (e) {
+                const forceLayout = newGraph.get('layoutController').layoutMethods[0];
+                forceLayout.execute();
+                refreshDragedNodePosition(e);
+            });
+            newGraph.on('node:dragend', function (e) {
+                if (!e.item) {
+                    return;
+                }
+                e.item.get('model').fx = null;
+                e.item.get('model').fy = null;
+            });
+
         }
     }, [ref.current]);
 
