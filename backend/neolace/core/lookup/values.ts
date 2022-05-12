67b2d985617fcfadbe229ccac097d1af8bb42f04
Braden MacDonald <mail@bradenm.com>
2022-05-11 19:30:29 -0700

    feat: Allow [list, of, entries] to act like an entry set for graph() etc

--- backend/neolace/core/lookup/values.ts
+++ backend/neolace/core/lookup/values.ts
@@ -900,6 +900,70 @@ export class LazyIterableValue extends LazyValue implements IIterableValue {
             sourceExpressionEntryId: this.sourceExpressionEntryId,
         });
     }
+
+    protected override async doCastTo(
+        newType: ClassOf<LookupValue>,
+        context: LookupContext,
+    ): Promise<LookupValue | undefined> {
+        if (newType === LazyEntrySetValue) {
+            // Special case handling to allow things like [entry1, entry2]
+            // or [this, this.ancestors(), this.descendants()] to act like a combined entry set, e.g. to graph them all.
+            // Tests for this are in ancestors.test.ts
+
+            const entryIds: VNID[] = [];
+            const entryQueries: CypherQuery[] = [];
+
+            const pageSize = 100n; // Load 100 values from this iterable at a time.
+            for (let i = 0n; true; i++) {
+                const slice = await this.getSlice(pageSize * i, pageSize);
+                // For each item in this iterable/list:
+                for (const value of slice) {
+                    // Is this a single entry?
+                    const valueAsEntry = await value.castTo(EntryValue, context);
+                    if (valueAsEntry) {
+                        // This value is a single entry. Add it to our set of entry IDs.
+                        entryIds.push(valueAsEntry.id);
+                        continue;
+                    }
+                    // Or is it a set of entries, represented by a query?
+                    const valueAsEntrySet = await value.castTo(LazyEntrySetValue, context);
+                    if (valueAsEntrySet) {
+                        entryQueries.push(valueAsEntrySet.cypherQuery);
+                        continue;
+                    }
+                    // Otherwise, we can't convert this iterable to a set of entries unfortunately.
+                    throw new LookupEvaluationError(
+                        `The iterable contains a value "${value.asLiteral()}" which does not represent an entry or entry set.`,
+                    );
+                }
+                if (slice.length < pageSize) {
+                    break;
+                }
+            }
+
+            let query = C`
+                CALL {
+                    MATCH (entry:${Entry})-[:${Entry.rel.IS_OF_TYPE}]->(et:${EntryType})-[:${EntryType.rel.FOR_SITE}]->(:${Site} {id: ${context.siteId}})
+                    WHERE entry.id IN ${entryIds}
+                    RETURN entry
+            `;
+            for (const q of entryQueries) {
+                query = C`
+                    ${query}
+                    UNION
+                    ${q}
+                    RETURN entry
+                `;
+            }
+            query = C`${query}
+                }
+                WITH entry, {} AS annotations
+            `;
+
+            return new LazyEntrySetValue(context, query);
+        }
+        return undefined;
+    }
 }
 
 /**
