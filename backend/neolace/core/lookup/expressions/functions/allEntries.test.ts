b55fdca5cfdd9d17a8171c6925942173b9bbe9d6
Braden MacDonald <mail@bradenm.com>
2022-05-23 19:26:13 -0700

    Add a new lookup function: allEntries()

---
+++ backend/neolace/core/lookup/expressions/functions/allEntries.test.ts
@@ -0,0 +1,56 @@
+import { VNID } from "neolace/deps/vertex-framework.ts";
+import { assert, assertEquals, group, setTestIsolation, test, TestLookupContext } from "neolace/lib/tests.ts";
+import { getGraph } from "neolace/core/graph.ts";
+import { CreateSite } from "neolace/core/Site.ts";
+import { ApplyEdits } from "neolace/core/edit/ApplyEdits.ts";
+import { EntryValue, IntegerValue, PageValue } from "../../values.ts";
+import { AllEntries } from "./allEntries.ts";
+
+group("allEntries.ts", () => {
+    const defaultData = setTestIsolation(setTestIsolation.levels.DEFAULT_ISOLATED);
+
+    test("Gives all entries from a site", async () => {
+        const graph = await getGraph();
+
+        // Create another site with three entries:
+        const otherSiteId = VNID(), entryType = VNID(), A = VNID(), B = VNID(), C = VNID();
+        await graph.runAsSystem(
+            CreateSite({ id: otherSiteId, name: "Test Site", domain: "test-site.neolace.net", slugId: "site-test" }),
+        );
+        await graph.runAsSystem(ApplyEdits({
+            siteId: otherSiteId,
+            edits: [
+                { code: "CreateEntryType", data: { id: entryType, name: "EntryType" } },
+                {
+                    code: "CreateEntry",
+                    data: { id: A, name: "Entry A", type: entryType, friendlyId: "a", description: "" },
+                },
+                {
+                    code: "CreateEntry",
+                    data: { id: B, name: "Entry B", type: entryType, friendlyId: "b", description: "" },
+                },
+                {
+                    code: "CreateEntry",
+                    data: { id: C, name: "Entry C", type: entryType, friendlyId: "c", description: "" },
+                },
+            ],
+        }));
+
+        const plantDbContext = new TestLookupContext({ siteId: defaultData.site.id });
+        const otherSiteContext = new TestLookupContext({ siteId: otherSiteId });
+
+        const numPlantDbEntries = Object.keys(defaultData.entries).length;
+        assertEquals(
+            await plantDbContext.evaluateExprConcrete("allEntries().count()"),
+            new IntegerValue(numPlantDbEntries),
+        );
+        assertEquals(
+            await otherSiteContext.evaluateExprConcrete("allEntries().count()"),
+            new IntegerValue(3),
+        );
+        const result = await otherSiteContext.evaluateExprConcrete(new AllEntries());
+        assert(result instanceof PageValue);
+        const IDs = result.values.map((v) => (v as EntryValue).id);
+        assertEquals(new Set(IDs), new Set([A, B, C]));
+    });
+});
---
+++ backend/neolace/core/lookup/expressions/functions/allEntries.ts
@@ -0,0 +1,30 @@
+import { C } from "neolace/deps/vertex-framework.ts";
+import { Entry } from "neolace/core/entry/Entry.ts";
+import { EntryType } from "neolace/core/schema/EntryType.ts";
+import { Site } from "neolace/core/Site.ts";
+
+import { LazyEntrySetValue, LookupValue } from "../../values.ts";
+import { LookupContext } from "../../context.ts";
+import { LookupFunction } from "./base.ts";
+
+/**
+ * allEntries(): Return all entries on the current site [that the user has permission to see].
+ */
+export class AllEntries extends LookupFunction {
+    static functionName = "allEntries";
+
+    public async getValue(context: LookupContext): Promise<LookupValue> {
+        return new LazyEntrySetValue(
+            context,
+            C`
+                MATCH (entry:${Entry})-[:${Entry.rel.IS_OF_TYPE}]->(et:${EntryType})-[:${EntryType.rel.FOR_SITE}]->(:${Site} {id: ${context.siteId}})
+
+                WITH entry, {} AS annotations
+            `,
+            {
+                sourceExpression: this,
+                sourceExpressionEntryId: context.entryId,
+            },
+        );
+    }
+}
