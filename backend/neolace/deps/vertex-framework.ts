8cd637c08c092aabd0bec4dfb6ef31cb91bfa4c7
Braden MacDonald <mail@bradenm.com>
2022-07-02 21:18:25 -0700

    Bump to Neo4j 4.4, Deno std 0.146

--- backend/neolace/deps/vertex-framework.ts
+++ backend/neolace/deps/vertex-framework.ts
@@ -1,3 +1,3 @@
 // For local dev:
 // export * from "../../../../vertex-framework/vertex/index.ts";
-export * from "https://raw.githubusercontent.com/neolace-dev/vertex-framework/ff54b15b89e866caf4d189ff2cb96308cd85a785/vertex/index.ts";
+export * from "https://raw.githubusercontent.com/neolace-dev/vertex-framework/307fb1c99108251449fffc1c3b2a2c3d165b990f/vertex/index.ts";
