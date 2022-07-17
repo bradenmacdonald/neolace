5168bb1b19157cb5489f854158ea11a3d4c9f733
Braden MacDonald <mail@bradenm.com>
2022-07-17 15:15:23 -0700

    Fix the migrate script

--- backend/neolace/scripts/migrate.ts
+++ backend/neolace/scripts/migrate.ts
@@ -1,8 +1,9 @@
 /**
  * Apply migrations to the Neo4j database (and anything else)
  */
-import { graph } from "neolace/core/graph.ts";
+import { getGraph } from "neolace/core/graph.ts";
 import { shutdown } from "../app/shutdown.ts";
 
+const graph = await getGraph();
 await graph.runMigrations();
 await shutdown();
