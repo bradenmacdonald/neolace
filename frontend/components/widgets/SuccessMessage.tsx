4cb2082b05a21d064684dd58c467ef63238be599
Braden MacDonald <mail@bradenm.com>
2022-08-07 16:53:42 -0700

    Fix React error when an error message is used inline in a paragraph

--- frontend/components/widgets/SuccessMessage.tsx
+++ frontend/components/widgets/SuccessMessage.tsx
@@ -10,11 +10,11 @@ interface Props {
  */
 export const SuccessMessage: React.FunctionComponent<Props> = (props: Props) => {
     return (
-        <div className="bg-green-50 border-green-800 border-2 px-2 py-1 rounded-md pl-8">
+        <span className="block bg-green-50 border-green-800 border-2 px-2 py-1 rounded-md pl-8">
             <span className="text-green-800 -ml-6 pr-1">
                 <Icon icon="check-circle-fill" />
             </span>{" "}
             {props.children}
-        </div>
+        </span>
     );
 };
