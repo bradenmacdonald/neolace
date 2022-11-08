8bc6a1bf1d1a226d92beae4d2c4b9af6248fd936
Braden MacDonald <mail@bradenm.com>
2022-11-07 19:05:33 -0800

    chore(backend): upgrade to latest release of vertex-framework

--- backend/neolace/deps/vertex-framework.ts
+++ backend/neolace/deps/vertex-framework.ts
@@ -1,3 +1,3 @@
 // For local dev:
 // export * from "../../../../vertex-framework/vertex/index.ts";
-export * from "https://raw.githubusercontent.com/neolace-dev/vertex-framework/f04965d8c7024141a3fcc88296a42ac9218ff4d7/vertex/index.ts";
+export * from "https://raw.githubusercontent.com/neolace-dev/vertex-framework/49d7d76c304ef10cc85cd64ce51e280653552b35/vertex/index.ts";
