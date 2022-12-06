99c55cc7f19ad6f85fe7282147d829b635b6b35f
Braden MacDonald <mail@bradenm.com>
2022-12-06 13:28:27 -0800

    fix(frontend): some bugs seen on the CAMS site

--- frontend/plugins/cams/plugin-pages/early-papers.tsx
+++ frontend/plugins/cams/plugin-pages/early-papers.tsx
@@ -6,7 +6,7 @@ import { LookupEvaluatorWithPagination } from "components/widgets/LookupEvaluato
 import { MDTContext } from "components/markdown-mdt/mdt";
 
 const expr = `
-    allEntries().filter(entryType=entryType("_42oypp3JCf4wpJRru0El17")).withDetail(prop=prop("_37i9jG21DNV0na1TAak57r")).sort(by=(e -> e.detail), reverse=true)
+    allEntries().filter(entryType=entryType("early-paper")).withDetail(prop=prop("date-published")).sort(by=(e -> e.detail), reverse=true)
 `;
 
 const PostersPresentations: React.FunctionComponent<PluginPageProps> = function (props) {
