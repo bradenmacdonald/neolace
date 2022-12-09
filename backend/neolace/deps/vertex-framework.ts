4041cd35eb2316d9e38db8d00981e50ac822995f
Braden MacDonald <mail@bradenm.com>
2022-12-08 19:02:49 -0800

    fix: in our production/stage environments, data was loading slowly

--- backend/neolace/deps/vertex-framework.ts
+++ backend/neolace/deps/vertex-framework.ts
@@ -1,3 +1,3 @@
 // For local dev:
 // export * from "../../../../vertex-framework/vertex/index.ts";
-export * from "https://raw.githubusercontent.com/neolace-dev/vertex-framework/8a4a2f7d803e02bd257f48e2c2a996cb961f74b6/vertex/index.ts";
+export * from "https://raw.githubusercontent.com/neolace-dev/vertex-framework/577f532a3fb9eec5f2aaf83a48c7de01471bc1ae/vertex/index.ts";
