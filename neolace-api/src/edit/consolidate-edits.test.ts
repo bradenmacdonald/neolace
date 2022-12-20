ab5946ad73b8eb6a90fc2bd0f19e3a8db3b1abaa
Braden MacDonald <mail@bradenm.com>
2022-12-20 12:42:02 -0800

    chore(api): Update dependencies (deno std, dnt)

--- neolace-api/src/edit/consolidate-edits.test.ts
+++ neolace-api/src/edit/consolidate-edits.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.146.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.170.0/testing/asserts.ts";
 import { VNID } from "../types.ts";
 import { AddPropertyFact, DeletePropertyFact, UpdatePropertyFact } from "./ContentEdit.ts";
 import {
