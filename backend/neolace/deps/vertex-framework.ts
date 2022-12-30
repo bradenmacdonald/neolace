d661cac0a92382247a9f2aa9c0c786660401af1b
Braden MacDonald <mail@bradenm.com>
2022-12-30 12:05:53 -0800

    fix(backend): Update Vertex Framework so test data snapshot works with non-constraint indexes

--- backend/neolace/deps/vertex-framework.ts
+++ backend/neolace/deps/vertex-framework.ts
@@ -1,3 +1,3 @@
 // For local dev:
 // export * from "../../../../vertex-framework/vertex/index.ts";
-export * from "https://raw.githubusercontent.com/neolace-dev/vertex-framework/3fe6c7e387d801605a6735de4756e3aed088db06/vertex/index.ts";
+export * from "https://raw.githubusercontent.com/neolace-dev/vertex-framework/48d6ce0d7e6cd7fd12615ef98a917d9a7618f899/vertex/index.ts";
