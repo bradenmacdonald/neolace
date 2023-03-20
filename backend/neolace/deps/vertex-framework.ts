3e501437c5c85039b40361d7d2bfdd4e293c8369
Braden MacDonald <mail@bradenm.com>
2023-03-19 17:02:56 -0700

    chore(backend): Bump vertex-framework to latest version (better logging)

--- backend/neolace/deps/vertex-framework.ts
+++ backend/neolace/deps/vertex-framework.ts
@@ -1,3 +1,3 @@
 // For local dev:
 // export * from "../../../../vertex-framework/vertex/index.ts";
-export * from "https://raw.githubusercontent.com/neolace-dev/vertex-framework/0a496e7dadcdb5899767def361a2415b0ed45544/vertex/index.ts";
+export * from "https://raw.githubusercontent.com/neolace-dev/vertex-framework/49aaac0fcacb2e1f5223915754bc124a840805c7/vertex/index.ts";
